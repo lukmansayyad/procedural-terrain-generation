872711542c5a576cb0448e87a5c9738fc76b4e28
lukmansayyad <lukmansayyad8@gmail.com>
2023-05-17 09:30:43 +0530

    ok

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -1,23 +1,21 @@
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
 
-
 [CustomEditor (typeof (MapGenerator))]
-public class NewBehaviourScript : Editor
-{
-  public override void OnInspectorGUI(){
-  MapGenerator mapGen = (MapGenerator)target;
-  
-  if (DrawDefaultInspector ()){
-  if (mapGen.autoUpdate ){
-      mapGen.GenerateMap ();
-  }
-  }
-  
-  if (GUILayout.Button ("Generate")){
-  	mapGen.GenerateMap ();
-  }
-}
+public class MapGeneratorEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		MapGenerator mapGen = (MapGenerator)target;
+
+		if (DrawDefaultInspector ()) {
+			if (mapGen.autoUpdate) {
+				mapGen.DrawMapInEditor ();
+			}
+		}
+
+		if (GUILayout.Button ("Generate")) {
+			mapGen.DrawMapInEditor ();
+		}
+	}
 }
